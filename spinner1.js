37d29d7f23eb62c5e5d5e353bf8fe00d167790e6
Ishmeet Kaur <67597941+ishpiamazing@users.noreply.github.com>
2020-08-24 13:25:37 -0400

    Update spinner1.js

--- spinner1.js
+++ spinner1.js
@@ -1,6 +1,6 @@
 //process.stdout.write('hello from spinner1.js... \rheyyy\n');
 
-setTimeout(()=> process.stdout.write('\r|    '), 100);
+setTimeout(()=> { process.stdout.write('\r|    ')}, 100);
 
 setTimeout(() => {
   process.stdout.write('\r/   ');
